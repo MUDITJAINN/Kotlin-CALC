f1f9568325143d622ca44685aab46b6dbcfec7b6
Mudit jain <muditj214@gmail.com>
2024-01-01 19:05:40 +0530

    used onclicklistner and evaluated the result

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -9,34 +9,50 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
-    lateinit var btnAdd : Button
+    lateinit var btnAdd : Button //initialized variable with their views
     lateinit var btnSub : Button
     lateinit var btnMul : Button
     lateinit var btnDiv : Button
     lateinit var etA : EditText
     lateinit var etB : EditText
-    lateinit var result : TextView
+    lateinit var resultTv : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnAdd = findViewById(R.id.btn_add)
+        btnAdd = findViewById(R.id.btn_add) //connection to id in activity_main.xml
         btnSub = findViewById(R.id.btn_subtraction)
         btnMul = findViewById(R.id.btn_multiplication)
         btnDiv = findViewById(R.id.btn_division)
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+        resultTv = findViewById(R.id.resulttv)
 
-
-
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMul.setOnClickListener(this)
+        btnDiv.setOnClickListener(this)
     }
-
     override fun onClick(v: View?) {
 
+        var a = etA.text.toString().toDouble()
+        var b = etB.text.toString().toDouble()
+        var result = 0.0
         when(v?.id)
         {
-            R.id.btn_add ->{}
-            R.id.btn_subtraction ->{}
-            R.id.btn_multiplication ->{}
-            R.id.btn_division ->{}
+            R.id.btn_add ->{
+                result = a+b
+            }
+            R.id.btn_subtraction ->{
+                result = a-b
+            }
+            R.id.btn_multiplication ->{
+                result = a*b
+            }
+            R.id.btn_division ->{
+                result = a/b
+            }
         }
+        resultTv.text="Result is $result"
     }
 }
