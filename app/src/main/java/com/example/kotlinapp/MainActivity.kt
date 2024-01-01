f8b3f61b68a08dec869525c55083bcec1040fd9f
Mudit jain <muditj214@gmail.com>
2024-01-01 18:36:42 +0530

    Initialized button in kotlin and connected them using id

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,16 +2,41 @@ package com.example.kotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
 
-
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMul : Button
+    lateinit var btnDiv : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var result : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_subtraction)
+        btnMul = findViewById(R.id.btn_multiplication)
+        btnDiv = findViewById(R.id.btn_division)
 
 
 
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v?.id)
+        {
+            R.id.btn_add ->{}
+            R.id.btn_subtraction ->{}
+            R.id.btn_multiplication ->{}
+            R.id.btn_division ->{}
+        }
     }
 }
